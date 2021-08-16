8e32de65a1add1e1147ce62565a2635cebcc9dd4
hemaro <hemaro@aistechnolabs.biz>
2021-08-16 17:18:54 +0530

    commit

--- src/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => config('settings.middleware')], function () {
-    Route::resource(config('settings.route'), 'PREPRV2\Settings\App\Http\Controllers\SettingsController');
-    Route::get(config('settings.route') . '/download/{setting}', 'PREPRV2\Settings\App\Http\Controllers\SettingsController@fileDownload');
-});
