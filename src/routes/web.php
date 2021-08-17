eced8f52f8cff4c5046bf929322cf7f472cef964
hemaro <hemaro@aistechnolabs.biz>
2021-08-17 15:41:06 +0530

    commit

--- src/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => config('settings.middleware')], function () {
-    Route::resource(config('settings.route'), 'HEMARAO\LaravelSettings\App\Http\Controllers\SettingsController');
-    Route::get(config('settings.route') . '/download/{setting}', 'HEMARAO\LaravelSettings\App\Http\Controllers\SettingsController@fileDownload');
-});
